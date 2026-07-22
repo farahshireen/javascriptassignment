d7fcff502fb86f7887ce41c8c07fb9c9ea1bf30c
farahshireen <syedafarahvi@gmail.com>
2026-07-23 01:48:09 +0500

    done

---
+++ chapter4/app.js
@@ -0,0 +1,31 @@
+
+
+// Q1: Declare 3 variables in one statement
+var name = "Ali", age = 20, city = "Karachi";
+
+// Q2: 5 Legal Variable Names
+var myName;
+var _name;
+var $name;
+var firstName;
+var name1;
+
+// 5 Illegal Variable Names
+
+// var 1name;
+// var my-name;
+// var my name;
+// var var;
+// var @name;
+
+// Q3: Display in Browser
+
+document.write("<h1>Rules for naming JS variables</h1>");
+
+document.write("Variable names can only contain , numbers, $ and _. For example: $my_1stVariable<br />");
+
+document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name<br />");
+
+document.write("Variable names are case sensitive.<br />");
+
+document.write("Variable names should not be JS keywords<br />.");
