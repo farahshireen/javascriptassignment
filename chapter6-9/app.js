49e60c05f39c02228bf59610c44c3afb6a81d908
farahshireen <syedafarahvi@gmail.com>
2026-07-25 00:57:57 +0500

    done

---
+++ chapter6-9/app.js
@@ -0,0 +1,34 @@
+// Question 1
+
+var a = 10
+
+document.write("Result:<br><br>");
+document.write("The value of a is: " + a + "<br>");
+document.write("......................................<br><br>");
+document.write("The value of ++a is " + (++a) + "<br>");
+document.write("Now the number of a is " + a + "<br>");
+document.write("The value of a++ is " + (a++) + "<br>")
+document.write("The value of ++a is " + a + "<br>");
+document.write("The value of --a is " + (--a) + "<br>");
+document.write("Now the number of a is " + a + "<br>");
+document.write("The value of a-- is " + (a--) + "<br>");
+document.write("Now the number of a is " + a + "<br>");
+
+// Question 2
+
+var a = 2
+var b = 1
+var result = --a - --b + ++b + b--
+document.write("a is " + a + "<br>")
+document.write("b is " + b + "<br>")
+document.write("Result is " + result +  "<br>")
+
+// Question 3
+
+var name = prompt("Enter your name");
+alert ("Welcome " + name)
+
+// Question 4
+
+
+
